dc6a0a55e9c138803102697e62d61cf9706641e4
VMKCreeper <92611907+MaxHuang2037@users.noreply.github.com>
2024-06-27 13:25:03 -0400

    Fixed responsiveness for things to do page

--- client/src/components/ThingsToDo/Activity.jsx
+++ client/src/components/ThingsToDo/Activity.jsx
@@ -4,13 +4,13 @@ export const Activity = ({offers}) => {
     console.log(offers)
     return(
         <div className={styles.activity}>
+            <div className={styles.right}>
+                <img className={styles.activity_picture} src={offers.pictures[0]} alt={offers.name}/>
+            </div>
             <div className={styles.left}>
                 <h2 className={styles.title}>{offers.name}</h2>
                 {(offers.price.amount != undefined && offers.price.amount != "0.0") && <h3 className={styles.amount}>${offers.price.amount} {offers.price.currencyCode}</h3>}
                 <p className={styles.description}>{offers.description}</p>
-            </div>
-            <div className={styles.right}>
-                <img className={styles.activity_picture} src={offers.pictures[0]} alt={offers.name}/>
             </div>
         </div>
     )
